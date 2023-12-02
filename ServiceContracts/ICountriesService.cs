1ee1f052a6e430a6756ba3cff88dcf6e2e8feca9
Glaybson Melo <gleybsonmelo998@gmail.com>
2023-12-02 09:53:23 -0300

    feat: add ServiceContracts for CountriesService and DTO

---
+++ ServiceContracts/ICountriesService.cs
@@ -0,0 +1,15 @@
+﻿namespace ServiceContracts;
+using ServiceContracts.DTO;
+
+/// <summary>
+/// Represents business logic for manipulating Country entity
+/// </summary>
+public interface ICountriesService
+{
+    /// <summary>
+    /// Adds a country object to the list of countries
+    /// </summary>
+    /// <param name="countryAddRequest">Country object to add</param>
+    /// <returns>Return country object after adding it (including newly generated country id)</returns>
+    CountryResponse AddCountry(CountryAddRequest? countryAddRequest);
+}
