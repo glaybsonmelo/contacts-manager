eb8e76ccf02439e6cdc7ad1f365d9e8d3113ec4f
Glaybson Melo <gleybsonmelo998@gmail.com>
2024-01-08 10:22:47 -0300

    feat: logging

--- CRUDExample/Program.cs
+++ CRUDExample/Program.cs
@@ -1,9 +1,20 @@
 using Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using ServiceContracts;
 using Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Logging
+builder.Host.ConfigureLogging(loggingProvider =>
+{
+    loggingProvider.ClearProviders();
+    loggingProvider.AddConsole();
+    loggingProvider.AddDebug();
+    loggingProvider.AddEventLog();
+});
+
 builder.Services.AddControllersWithViews();
 
 //add services into IoC container
@@ -16,12 +27,18 @@ builder.Services.AddDbContext<PersonsDbContext>(options =>
     }
 );
 
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
+app.Logger.LogDebug("debug-message");
+app.Logger.LogInformation("information-message");
+app.Logger.LogWarning("warning-message");
+app.Logger.LogError("error-message");
+app.Logger.LogCritical("critical-message");
 
 Rotativa.AspNetCore.RotativaConfiguration
     .Setup("wwwroot", wkhtmltopdfRelativePath: "Rotativa");
