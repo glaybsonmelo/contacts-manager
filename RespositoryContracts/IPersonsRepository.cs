3441dc13ce9719ebc03e9d48bb4e18e2882721ba
Glaybson Melo <gleybsonmelo998@gmail.com>
2024-01-14 17:02:21 -0300

    refactor: added IPersonsRepository

--- RespositoryContracts/IPersonsRepository.cs
+++ RespositoryContracts/IPersonsRepository.cs
@@ -1,12 +1,22 @@
-﻿using System;
+﻿using Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RespositoryContractsS
+namespace RespositoryContracts
 {
-    internal class IPersonsRepository
+    /// <summary>
+    /// Represents data acess logic for managing Person entity
+    /// </summary>
+    public interface IPersonsRepository
     {
+        /// <summary>
+        /// Add person object to the data store
+        /// </summary>
+        /// <param name="person">person object to add</param>
+        /// <returns>the Person object after adding it to the table</returns>
+        Task<Person> AddPerson(Person person);
     }
 }
