8fb399415153206670ea2915b071e18a9f239eb4
YourUsername <gleybsonmelo998@gmail.com>
2024-01-20 23:13:41 -0300

    test: unit test for PersonsController method Index

--- CRUDTests/PersonsControllerTest.cs
+++ CRUDTests/PersonsControllerTest.cs
@@ -1,13 +1,17 @@
 ﻿using AutoFixture;
 using Moq;
 using ServiceContracts;
+using ServiceContracts.Enums;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-
+using FluentAssertions;
+using CRUDExample.Controllers;
+using ServiceContracts.DTO;
+using Microsoft.AspNetCore.Mvc;
 namespace CRUDTests
 {
     public class PersonsControllerTest 
@@ -20,7 +24,7 @@ namespace CRUDTests
 
         private readonly Fixture _fixture;
 
-        public PersonsControllerTest(ICountriesService countriesService)
+        public PersonsControllerTest()
         {
             _fixture = new Fixture();
             _personsServiceMock = new Mock<IPersonsService>();
@@ -33,7 +37,25 @@ namespace CRUDTests
         [Fact]
         public async Task Index_ShouldReturnIndexViewWithPersonsList()
         {
+            
+            List<PersonResponse> person_response_list = _fixture.Create<List<PersonResponse>>();
+            PersonsController personController = new PersonsController(_personsService, _countriesService);
 
+            _personsServiceMock.Setup(temp => temp.GetFilteredPersons(It.IsAny<string>(), It.IsAny<string>())).ReturnsAsync(person_response_list);
+            _personsServiceMock.Setup(temp => temp.GetSortedPersons(It.IsAny<List<PersonResponse>>(), It.IsAny<string>(), It.IsAny<SortOrderOptions>())).ReturnsAsync(person_response_list);
+
+            //Act
+            IActionResult result = await personController.Index(
+                _fixture.Create<string>(), 
+                _fixture.Create<string>(), 
+                _fixture.Create<string>(), 
+                _fixture.Create<SortOrderOptions>()
+                );
+            //Assert
+            ViewResult viewResult = Assert.IsType<ViewResult>(result);
+            //ViewResult viewResult =  result.Should().BeOfType<ViewResult>();
+            viewResult.ViewData.Model.Should().BeAssignableTo<List<PersonResponse>>();
+            viewResult.ViewData.Model.Should().Be(person_response_list);
         }
         #endregion
 
