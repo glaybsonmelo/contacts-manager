28411cd1c9b80b3bdc563b764c63e6da3050e833
YourUsername <gleybsonmelo998@gmail.com>
2024-02-08 22:10:38 -0300

    refactor: ocp PersonsService implements

---
+++ Services/PersonsAdderService.cs
@@ -0,0 +1,42 @@
+﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using Entities;
+using Services.Helpers;
+using ServiceContracts.Enums;
+using RespositoryContracts;
+using Exceptions;
+
+namespace Services
+{
+    public class PersonsAdderService : IPersonsAdderService
+    {
+        private readonly IPersonsRepository _personsRepository;
+
+        public PersonsAdderService(IPersonsRepository personsRepository)
+        {
+            _personsRepository = personsRepository;
+        }
+
+        public async Task<PersonResponse> AddPerson(PersonAddRequest? personAddRequest)
+        {
+            if (personAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(personAddRequest));
+            }
+
+            // Model Validation
+            ValidationHelper.ModelValidation(personAddRequest);
+
+            Person? person = personAddRequest.ToPerson();
+
+            person.Id = Guid.NewGuid();
+
+            Person ps = await _personsRepository.AddPerson(person);
+
+            return ps.ToPersonResponse();
+
+        }
+
+    }
+}
+
